3c77b8e0212fde74014763a49fda182e2d4e18b9
MauricioZir <mauricionzir@gmail.com>
2025-05-15 16:47:16 -0300

    a

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -18,76 +18,72 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         apellido=update.message.from_user.last_name
     else:
         apellido=""
-    kb = [["temperatura"],["humedad"],["gráfico temperatura"],["gráfico humedad"]]
+    kb = [["Destello"],["Modo"],["Relé"]]
     await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
 
 async def acercade(update: Update, context):
     await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
 
-async def kill(update: Update, context):
-    logging.info(context.args)
-    if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAOPZkuctzsWZVlDSNoP9PavSZmH5poAAmUCAALrx0lEVKaX7K-68Ns1BA")
-        await asyncio.sleep(6)
-        await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
+
+async def update_setpoint(update: Update, context):
+    # Verifica si se paso el setpoint como parámetro.
+    if not context.args:
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="⚠️ Falta indicar el valor del setpoint."
+        )
+        return
+
+    # Verifica si el setpoint es un número.
+    try:
+        setpoint = float(context.args[0])
+    except ValueError:
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="⚠️ El valor del setpoint debe ser un número válido."
+        )
+        return
+
+    # Si existe y es número, continúa con el resto
+    await context.bot.send_message(
+        chat_id=update.message.chat.id,
+        text=f"✅ Cambiando SetPoint a {setpoint} °C."
+    )
+
+
+async def button_handler(update: Update, context):
+    mensaje = update.message.text
+    logging.info(f"Mensaje recibido: {mensaje}")
+
+    if mensaje == "Destello":
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="⚡ Se destellaaaaaaaaa"
+        )
+    elif mensaje == "Relé":
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="🔌 Activando el relé"
+        )
+    elif mensaje == "Modo":
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="🔄 Cambiando de modo"
+        )
     else:
-        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
-        
-async def medicion(update: Update, context):
-    logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text} FROM mediciones ORDER BY timestamp DESC LIMIT 1"
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
-    async with conn.cursor() as cur:
-        await cur.execute(sql)
-        r = await cur.fetchone()
-        if update.message.text == 'temperatura':
-            unidad = 'ºC'
-        else:
-            unidad = '%'
-        await context.bot.send_message(update.message.chat.id,
-                                    text="La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}"
-                                    .format(update.message.text, str(r[1]).replace('.',','), unidad, r[0]))
-        logging.info("La última {} es de {} {}, medida a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, r[1], unidad, r[0]))
-    conn.close()
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text="❓ Comando desconocido"
+        )
 
-async def graficos(update: Update, context):
-    logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text.split()[1]} FROM mediciones where id mod 2 = 0 AND timestamp >= NOW() - INTERVAL 1 DAY AND sensor_id LIKE 'sensor_1' ORDER BY timestamp"
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
-    async with conn.cursor() as cur:
-        await cur.execute(sql)
-        filas = await cur.fetchall()
-
-        fig, ax = plt.subplots(figsize=(7, 4))
-        fecha,var=zip(*filas)
-        ax.plot(fecha,var)
-        ax.grid(True, which='both')
-        ax.set_title(update.message.text, fontsize=14, verticalalignment='bottom')
-        ax.set_xlabel('fecha')
-        ax.set_ylabel('unidad')
-
-        buffer = BytesIO()
-        fig.tight_layout()
-        fig.savefig(buffer, format='png')
-        plt.close()
-        buffer.seek(0)
-        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=buffer)
-        buffer.close()
-    conn.close()
 
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('acercade', acercade))
-    application.add_handler(CommandHandler('kill', kill))
-    application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
-    application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
+    application.add_handler(CommandHandler('about', acercade))
+
+    application.add_handler(CommandHandler('setpoint', update_setpoint))
+    application.add_handler(MessageHandler(filters.Regex("^(Destello|Relé|Modo)$"), button_handler))
     application.run_polling()
 
 if __name__ == '__main__':
